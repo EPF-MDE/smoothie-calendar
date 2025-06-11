10d9c9bbb1743c9152b873f0a17749718c2849d4
celmds <celine.bernardinomendes@epfedu.fr>
2025-06-11 22:30:38 +0200

    changement des resource ID en int

--- packages/core/src/plugins/tree-resource-time-grid/View.svelte
+++ packages/core/src/plugins/tree-resource-time-grid/View.svelte
@@ -26,17 +26,19 @@
 <style>
     .cm-row {
         display: flex;
-        text-align: center;
-        font-weight: bold;
-        background: white;
         border-bottom: 1px solid var(--ec-border-color);
     }
 
     .cm-cell {
         flex: 1;
+        text-align: center;
         padding: 4px 0;
         border-right: 1px solid var(--ec-border-color);
         border-left: 1px solid var(--ec-border-color);
+    }
+    .cm-pair{
+        display: flex;
+        flex: 1;
     }
 
     .cm-cell:last-child {
@@ -103,17 +105,23 @@
                 {/each}
             </div>
 
-            <div class="cm-row">
-                {#each $_viewDates as _}
-                    <div class="cm-cell">CM</div>
-                {/each}
-            </div>
+        <div class="cm-row">
+    {#each $_viewDates as date}
+        <div class="cm-pair">
+            {#each $_viewResources.filter(r => r.id.startsWith('1')) as cmresource}
+                <div class="cm-cell">
+                    <Label resource={cmresource} date={date} />
+                </div>
+            {/each}
+        </div>
+    {/each}
+</div>
 
             <div class="dea-row">
               {#each $_viewDates as date}
                 <div class="dea-pair">
                   
-                  {#each $_viewResources.filter(r => r.id.startsWith('DEA') || r.id.startsWith('TD')) as resource}
+                  {#each $_viewResources.filter(r => r.id.startsWith('2')) as resource}
 
                     <div class="dea-cell">
                       <Label resource={resource} date={date} />
